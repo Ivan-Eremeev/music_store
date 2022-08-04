e3d0ac24f0be3f326630c28f77124f82b8b7d38e
Ivan <ivan.frontcoder@gmail.com>
2022-08-04 14:42:34 +0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -126,11 +126,6 @@ window.onload = function () {
     });
   }
 
-  // // Air Datepicker
-  // new AirDatepicker('#airDatepicker', {
-  //   position: 'right top',
-  // });
-
   // Magnific Popup
   $('.open-popup-link').magnificPopup({
     mainClass: 'mfp-fade',
@@ -160,66 +155,36 @@ window.onload = function () {
     }
   })
 
-  // // Табы
-	// function tabs() {
-  //   const tabs = $('.js-tabs');
-	// 	if (tabs.length) {
-	// 		tabs.each( function () {
-  //       let triggers = $(this).find('.js-tabs-trigger');
-  //       let contents = $(this).find('.js-tabs-content');
-  //       let time = 300;
-  //       triggers.on('click', function () {
-  //         let trigger = $(this);
-  //         let content = $('.js-tabs-content[data-href="' + trigger.attr('href') +'"]');
-  //         if (!trigger.hasClass('active')) {
-  //           triggers.removeClass('active');
-  //           trigger.addClass('active');
-  //           contents.hide();
-  //           contents.removeClass('open');
-  //           content.fadeIn(time, function () {
-  //             $(this).addClass('open');
-  //           });
-  //         }else {
-  //           return false;
-  //         }
-  //       })
-  //     });
-	// 	}
-	// }
-	// tabs();
-
-  // // Выпадайки при клике по кнопке
-	// // Задать блокам выпадайкам айдишник совпадающий с data-drop="" в кнопке для этого блока
-	// // Задать кнопкам .js-drop-btn и data-drop="" с айдишником блока выпадайки
-	// function dropBlock(btn) {
-	// 	var $this = undefined,
-	// 			drop = undefined,
-	// 			close = $('.js-drop-close');
-	// 	btn.on('click', function () {
-	// 		$this = $(this);
-	// 		drop = $('#' + $this.data('drop'));
-	// 		$this.toggleClass('is-active');
-	// 		drop.toggleClass('open');
-	// 		$(document).mouseup(function (e) {
-	// 			if (!$this.is(e.target)
-	// 				&& $this.has(e.target).length === 0
-	// 				&& !drop.is(e.target)
-	// 				&& drop.has(e.target).length === 0) {
-	// 				$this.removeClass('is-active');
-	// 				drop.removeClass('open');
-	// 			}
-	// 		});
-	// 	})
-	// 	close.on('click', function () {
-	// 		$('[data-drop="' + $(this).data('drop') +'"]').removeClass('is-active');
-	// 		$('#' + $(this).data('drop')).removeClass('open');
-	// 	})
-	// }
-	// dropBlock($('.js-drop-btn'));
+  // Табы
+	function tabs() {
+    const tabs = $('.js-tabs');
+		if (tabs.length) {
+			tabs.each( function () {
+        let triggers = $(this).find('.js-tabs-trigger');
+        let contents = $(this).find('.js-tabs-content');
+        let time = 300;
+        triggers.on('click', function (e) {
+          e.preventDefault();
+          let trigger = $(this);
+          let content = $('.js-tabs-content[data-href="' + trigger.attr('href') +'"]');
+          if (!trigger.hasClass('active')) {
+            triggers.removeClass('active');
+            trigger.addClass('active');
+            contents.hide();
+            contents.removeClass('open');
+            content.fadeIn(time, function () {
+              $(this).addClass('open');
+            });
+          }else {
+            return false;
+          }
+        })
+      });
+		}
+	}
+	tabs();
 
   // Выпадайки при клике по кнопке slide
-	// Задать блокам выпадайкам айдишник совпадающий с data-drop="" в кнопке для этого блока
-	// Задать кнопкам .js-drop-btn и data-drop="" с айдишником блока выпадайки
 	function dropBlockSlide(btn) {
 		var $this = undefined,
 				drop = undefined,
@@ -245,5 +210,32 @@ window.onload = function () {
 		})
 	}
 	dropBlockSlide($('.js-drop-btn-slide'));
+
+  // Выпадайки при фокусе по input
+  function dropBlockFocus(btn) {
+    var $this = undefined,
+      drop = undefined,
+      close = $('.js-drop-close');
+    btn.on('focus', function () {
+      $this = $(this);
+      drop = $('#' + $this.data('drop'));
+      $this.addClass('is-active');
+      drop.stop().slideDown(200);
+      $(document).mouseup(function (e) {
+        if (!$this.is(e.target)
+          && $this.has(e.target).length === 0
+          && !drop.is(e.target)
+          && drop.has(e.target).length === 0) {
+          $this.removeClass('is-active');
+          drop.stop().slideUp(200);
+        }
+      });
+    })
+    close.on('click', function () {
+      $('[data-drop="' + $(this).data('drop') + '"]').removeClass('is-active');
+      $('#' + $(this).data('drop')).stop().slideUp(200);
+    })
+  }
+  dropBlockFocus($('.js-drop-input'));
 
 }
